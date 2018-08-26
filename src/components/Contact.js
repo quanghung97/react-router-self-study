f5731ce959236918a84520ef217e343d7de6c3cd
quanghung97 <hungcan1997@gmail.com>
2018-08-26 16:01:36 +0700

    object prompt authenticate user to go other page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,33 @@
 import React, {Component} from 'react';
+import { Prompt } from 'react-router-dom';
 
 class Contact extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            isChecked : false
+        }
+    }
+
+    onClick = (isChecked) => {
+        this.setState({
+            isChecked : isChecked
+        });
+    }
+
     render() {
+        var {isChecked} = this.state;
         return (
+
             <div>
                 Đây là liên hệ
+                <button type="button" className="btn btn-large btn-block btn-info" onClick={()=>this.onClick(false)}>Cho Phep</button> <br />
+                <button type="button" className="btn btn-large btn-block btn-danger" onClick={()=>this.onClick(true)}>Khong cho phep</button>
+                <Prompt
+                    when = {isChecked}
+                    message = { location => (`Ban chac chan di den ${location.pathname}`) }
+                />
             </div>
     );
     }
